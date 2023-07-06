e857d65f7fa2f62558655905dbabcf37288214b3
Rosa Kang <therosessom@gmail.com>
2023-07-06 14:00:01 -0700

    get videoId using regex

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import { ImageComponent } from "./components/item/image.js";
+import { NoteComponent } from "./components/item/note.js";
+import { TodoComponent } from "./components/item/todo.js";
 import { VideoComponent } from "./components/item/video.js";
 import { PageComponent } from "./components/page/page.js";// 확장명도 함께 호출해야함.
 
@@ -11,7 +13,13 @@ class App{
         const image = new ImageComponent('picture that AI chose randomly', 'https://picsum.photos/600/300');
         image.attachTo(appRoot, 'beforeend');
 
-        const video = new VideoComponent('favorite video', 'https://www.youtube.com/watch?v=wE2g_aJ8jW4');
+        const note = new NoteComponent('Note Title', 'Note Body');
+        note.attachTo(appRoot, 'beforeend');
+
+        const todo = new TodoComponent('Toto Title', 'Go for a walk');
+        todo.attachTo(appRoot, 'beforeend');
+
+        const video = new VideoComponent('favorite video', 'https://www.youtube.com/embed/8MURF7ihkqs');
         video.attachTo(appRoot, 'beforeend');
 
     }
--- src/components/item/video.ts
+++ src/components/item/video.ts
@@ -1,20 +1,30 @@
 import { BaseComponent } from "../component";
 
 export class VideoComponent extends BaseComponent<HTMLElement>{
-    
-    constructor(title:string, video:string) {
+    constructor(title:string, url:string) {
         super(`<section class="video">
-                <div class="video___holder">
-                    <video class="video__src"></video>
+                <div class="video___player">
+                    <iframe class="video__iframe"></iframe>
                 </div>
-                <p class="video__title"></p>
+                <h3 class="video__title"></h3>
             </section>`);
 
-        const VideoElement = this.element.querySelector('.video__src')! as HTMLVideoElement;
-        VideoElement.src = video;
+        const iframe = this.element.querySelector('.video__iframe')! as HTMLIFrameElement;
+        iframe.src = this.convertToEmbeddedURL(url);
 
-        const VideoTitle = this.element.querySelector('.video__title')! as HTMLElement;
-        VideoTitle.textContent = title;
+        const titleElement = this.element.querySelector('.video__title')! as HTMLHeadingElement;
+        titleElement.textContent = title;
 
+    }
+
+    private convertToEmbeddedURL(url: string): string {
+        //Regex(정규표현식)
+        const regExp = /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9-]{11}))|(?:youtu.be\/([a-zA-Z0-9-]{11})))/;
+        const match = url.match(regExp);
+        const videoId = match ? match[1] || match[2] : undefined;
+        if (videoId) {
+            return `https://www.youtube.com/embed/${videoId}`
+        }
+        return url;
     }
 }
