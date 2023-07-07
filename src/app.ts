2a46814500d871c847831a4ccafada00528f2f6c
Rosa Kang <therosessom@gmail.com>
2023-07-07 09:44:52 -0700

    edit app.ts

--- src/app.ts
+++ src/app.ts
@@ -46,30 +46,7 @@ class App{
     }
 
 
-        private bindElementToDialog<T extends (MediaData | TextData) & Component>(selector: string,
-        InputComponent: InputComponentConstructor<T>,
-        makeSection:(input: T) => Component
-        ) {
-            const element = document.querySelector(selector)! as HTMLButtonElement;
-            element.addEventListener('click', () => {
-                const dialog = new InputDialog();
-                const inputSection = new InputComponent();
-
-                dialog.addChild(inputSection);
-                dialog.attachTo(this.dialogRoot);
-
-                dialog.setOnCloseListener(() => {
-                    dialog.removeFrom(this.dialogRoot);
-                });
-
-                dialog.setOnSubmitListener(() => {
-                    const image = makeSection(input);
-                    this.page.addChild(image);
-                    dialog.removeFrom(this.dialogRoot);
-                });
-
-            })
-    }
+        
 }
 
 new App(document.querySelector('.document')! as HTMLElement, document.body);
